a02d0ba71785e06b5e2bcd630cf52fb3eeb09838
Elise <crimsoncrescentmoon@gmail.com>
2017-03-01 23:46:08 -0600

    lab3 broken

--- Lab3/LinearRegression.py
+++ Lab3/LinearRegression.py
@@ -8,18 +8,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# This dataset contains features in a very wide range. Best way to proceed will be to normalize the dataset(with standard deviation). After the data is loaded and normalized, include the intercept term in the dataframe.
-df = #Load and preprocess the dataset.
+# This dataset contains features in a very wide range. 
+# Best way to proceed will be to normalize the dataset(with standard deviation). 
+# After the data is loaded and normalized, include the intercept term in the dataframe.
+df = pd.read_csv('HousingData_LinearRegression.csv', dtype=np.float64) #Load and preprocess the dataset.
+for i in df:
+    mean = df[i].mean()
+    std = df[i].std()
+    df[i] = ( df[i] - mean )/std
 
-
-# 1. After you've added the intercept term, define X as the features in the dataframe. Define Y as the target variable.
+# print df
+# 1. After you've added the intercept term, 
+# define X as the features in the dataframe. Define Y as the target variable.
 # 2. Convert them to a numpy array and define beta(the coeffecients) with zeros.
-X = 
-Y = 
-X = np.matrix(X.values)  
-Y = np.matrix(Y.values)  
-beta = 
+X = df.drop(df.columns[len(df.columns)-1], axis=1 )
+# for i in X:
+#     mean = X[i].mean()
+#     std = X[i].std()
+#     X[i] = ( X[i] - mean )/std
+X.insert(0, "Design", 1.0)
 
+Y = df.iloc[:,-1]
+# mean = Y.mean(axis=0)
+# std = Y.std(axis=0)
+# Y = (Y-mean)/std
+
+X = np.matrix(X.values, dtype=np.float64)
+Y = np.matrix(Y.values, dtype=np.float64)
+beta = np.matrix(np.zeros((1,X.shape[1])), dtype=np.float64)
 
 # Since we now have every module to calculate our cost function, we'll go ahead and define it.
 def costFunction(X, Y, beta):
@@ -27,8 +43,12 @@ def costFunction(X, Y, beta):
     Compute the Least Square Cost Function.
     Return the calculated cost function.
     '''
+    element = np.power( ( ( X*beta.T ) - Y ), 2 )
+    cost = np.sum( element ) / ( 2 * len(X) )
+
     return cost
 
+#print costFunction (X, Y, beta)
 
 # Define a Gradient Descent method that will update beta in every iteration and also update the cost.
 def gradientDescent(X, Y, beta, alpha, iters):
@@ -36,17 +56,34 @@ def gradientDescent(X, Y, beta, alpha, iters):
     Compute the gradient descent function.
     Return beta and the cost array.
     '''
-    
-    return beta, cost
+    cost = []
 
+    m = len(X)
+    betaT = beta.T
+
+    # print X
+    # print betaT
+
+    for i in range(0, iters):
+        print (X * betaT)
+        betaT = betaT - (alpha/m)*(( (X * betaT) - Y.T).T * X).T #matrix solution
+        # hypothesis = np.dot(X, betaT)
+        # loss = hypothesis - Y.T
+        # gradient = np.dot(X.T, loss) / m
+        # print "gradient"
+        # print gradient
+        # betaT = betaT - alpha * gradient
+        beta = betaT.T
+        cost.append( costFunction(X, Y, beta) )
+
+    return beta, cost
 
 # Define alpha and number of iterations of your choice and use them to call to gradientDescent function.
 #please try different values to see the results, but alpha=0.01 and iters=1000 are suggested.
-alpha = #Define  
-iters = #Define
+alpha = 0.01 #Define  
+iters = 1000 #Define
 
 result = gradientDescent(X, Y, beta, alpha, iters)
-
 
 # Implement the Ridge Regression regularization and report the change in coeffecients of the parameters.
 def gradientDescentRidge(X, Y, beta, alpha, itersreg, ridgeLambda):
@@ -54,30 +91,53 @@ def gradientDescentRidge(X, Y, beta, alpha, itersreg, ridgeLambda):
     Compute the gradient descent function.
     Return beta and the cost array.
     '''
+    cost = []
+
+    m = len(X)
+
+    betaT = beta.T
+    YT = Y.T
+    for i in range(0, itersreg):
+        hypothesis = np.dot(X, betaT)
+        loss = hypothesis - YT
+        gradient = np.dot(X.T, loss)/m
+        betaT = betaT - alpha * ( gradient + 2 * ridgeLambda * np.sum(betaT) )
+        beta = betaT.T
+        cost.append(costFunction(X, Y, beta))
+
     return beta, cost
 
-
-# Define alpha, number of iterations and lambda of your choice that minimizes the cost function and use them to call to gradientDescent function. Plot the cost graph with iterations titled "Error vs training" with and without regularization(y axis labeled as cost and x axix labeled as iterations). Then, calculate the MSE.
+# Define alpha, number of iterations and lambda of your choice that minimizes the cost function and use them to 
+# call to gradientDescent function. Plot the cost graph with iterations titled "Error vs training" with and without 
+# regularization(y axis labeled as cost and x axix labeled as iterations). Then, calculate the MSE.
 def MSE(beta):
     '''
     Compute and return the MSE.
     '''
+    element = np.power( ( X*beta.T - Y ), 2 )
+    mse = np.sum( element ) / ( len(X) )
+
     return mse
 
-
-
-
 #Try differnt values, but the same values for alpha and itersreg are suggested, 0.05 for lambda is suggested
-alpha = #Define
-itersreg = #Define
-ridgeLambda = #Define
+alpha = alpha #Define
+itersreg = iters #Define
+ridgeLambda = 0.05 #Define
+beta.fill(0)
 regResult = gradientDescentRidge(X, Y, beta, alpha, itersreg, ridgeLambda)
-print regResult[0]
+
+plt.title("Error vs training")
+plt.ylabel("cost")
+plt.xlabel("iterations")
+plt.plot(result[1], color="red", label="regular")
+plt.plot(regResult[1], color="blue", label="ridge")
+plt.legend()
 
 #MSE for beta with regularization
-beta= #Define
-MSE(beta)
+beta = beta#Define
+print MSE(beta)
 
+plt.show()
 
 #The final result wanted for this portion of the lab is the last plot defined earlier, the explanation regarding the 
 #coeffecients of the parameters with Ridge Regression regularization, and the MSE. Please only include the
--- Lab3/LogisticRegression.py
+++ Lab3/LogisticRegression.py
@@ -2,33 +2,39 @@
 # coding: utf-8
 
 
-
+from __future__ import division
 import numpy as np
 import pandas as pd
 import scipy.optimize as opt
-from __future__ import division
-
 
 # Load the LogisticData.csv for this assignment and check if it has loaded successfully. In this excercise, we will use the same dataset for training and testing. After training the model on the best beta, we will see how well does our model perform.
-df = #define
+df = pd.read_csv('LogisticData.csv', dtype=np.float64)#define
 
 
-# Next, preprocess the data. X should contain only the predictors, Y shuould be the reponse variable and beta should be a vector with length = the number of features and value = 0.
-X =   
-Y = 
-groundTruth = df['Y']
+# Next, preprocess the data. X should contain only the predictors, Y shuould be the reponse variable and 
+# beta should be a vector with length = the number of features and value = 0.
+X = df.drop(df.columns[len(df.columns)-1], axis=1 )
+for i in X:
+    mean = X[i].mean(axis=0)
+    std = X[i].std(axis=0)
+    X[i] = ( X[i] - mean )/std
+X.insert(0, "Design", 1.0)
+Y = df.iloc[:,-1]  
+groundTruth = Y #df['Y']
+#print groundTruth
+
 #Normalize only if you are using gradient descent. Apply standard deviation for normalization.
+X = np.matrix(X.values, dtype=np.float64)  
+Y = np.matrix(Y.values, dtype=np.float64)
+beta = np.matrix(np.zeros((1,X.shape[1])), dtype=np.float64)
 
-X = np.matrix(X.values)  
-Y = np.matrix(Y.values)
-beta = 
-
-
-# Define a sigmoid function and return the value tht has been calculated for z
+# Define a sigmoid function and return the value that has been calculated for z
 def sigmoid(z):
     '''
     Here sigmoid value of 'z' needs to be returned.
     '''
+    sig = 1 / ( 1 + np.exp(-z) )
+
     return sig
 
 
@@ -37,8 +43,33 @@ def costFunction(beta, X, Y):
     '''
     This function returns the value computed from the cost function.
     '''
-    return result
+    costSum = 0
 
+    m = len(X)
+    print np.sum( np.dot(X[0,:], beta.T) ) 
+    for i in range(m):
+        hypothesis = 1 / ( 1 + np.exp(- np.sum( np.dot(X[i,:], beta.T) )) )
+        costSum += Y[:,i] * np.log( hypothesis ) + ( 1-Y[:,i] ) * ( np.log( 1 - hypothesis ) )
+    cost = - costSum/m
+    
+    return cost
+
+# # Now , only define the gradient function that we can use in the SciPy's optimize module to find the optimal betas. 
+def gradient(beta, X, Y):
+    '''
+    This function returns the gradient calucated.
+    '''
+    m = len(X)
+    grad = np.matrix(np.zeros((1,X.shape[1])), dtype=np.float64)
+
+    for i in range(m):
+        exponent = np.exp( - np.sum( np.dot( X[i,:], beta.T ) ) )
+        denom = 1 + exponent
+        grad += Y[:,i] * ( ( exponent*X[i,:] ) / denom ) + ( 1 - Y[:,i] ) * ( ( exponent*X[i,:] ) / ( np.power( denom, 2 ) - denom ) )
+    
+    grad = grad/m
+
+    return grad
 
 # Define a gradient function that takes in beta, X and Y as parameters and returns the best betas and cost. 
 def gradientDescent(X, Y, beta, alpha, iters):
@@ -46,33 +77,36 @@ def gradientDescent(X, Y, beta, alpha, iters):
     Compute the gradient descent function.
     Return the newly computed beta and the cost array.
     '''
+    cost = []
+
+    for i in range(3):
+        grad = gradient(beta, X, Y)
+        beta = beta - alpha * grad
+        cost.append(costFunction(beta, X, Y))
 
     return beta, cost
 
-
 # Try out multiple values of 'alpha' and 'iters' so that you get the optimum result.
 #please try different values to see the results, but alpha=0.01 and iters=10000 are suggested.
-alpha = #define
-iters = #define
+alpha = 0.01 #define
+iters = 1000 #define
 result = gradientDescent(X, Y, beta, alpha, iters)
 
 
-# Now , only define the gradient function that we can use in the SciPy's optimize module to find the optimal betas. 
-def gradient(beta, X, Y):
-    '''
-    This function returns the gradient calucated.
-    '''
-    #for i in range(parameters):
-    #####
-    #grad[i] =
-    return grad
+# # # Now , only define the gradient function that we can use in the SciPy's optimize module to find the optimal betas. 
+# def gradient(beta, X, Y):
+#     '''
+#     This function returns the gradient calucated.
+#     '''
+
+#     return grad
 
 
 # Optimize the parameters given functions to compute the cost and the gradients. We can use SciPy's optimization to do the same thing.
 # Define a variable result and complete the functions by adding the right parameters.
 
 #the optimised betas are stored in the first index of the result variable
-result = opt.fmin_tnc(func= , x0= , fprime = , args= )
+#result = opt.fmin_tnc(func= , x0= , fprime = , args= )
 
 
 # Define a predict function that returns 1 if the probablity of the result from the sigmoid function is greater than 0.5, using the best betas and 0 otherwise.
@@ -81,7 +115,13 @@ def predict(beta, X):
     '''
     This function returns a list of predictions calculated from the sigmoid using the best beta.
     '''
-    return #define
+    prediction = []
+
+    for i in range(len(X)):
+        hypothesis = 1 / ( 1 + np.exp(- np.sum( np.dot(X[i,:], beta.T) ) ) )
+        prediction.append( 1 if ( hypothesis > 0.5 ) else 0 )
+
+    return prediction#define
 
 
 # Store the prediction in a list after calling the predict function with best betas and X.
@@ -97,6 +137,21 @@ def confusionMatrix(prediction, groundTruth):
     '''
     return pd.crosstab(groundTruth, prediction, rownames=['True'], colnames=['Predicted'], margins=False)
 
+def accuracy(confusionMatrix):
+    '''
+    Calculates accuracy given the confusion matrix
+    '''
+    numerator = 0
+    denominator = 0
+
+    for index, row in confusionMatrix.iterrows():
+        for i in range(len(row)):
+            if(index == i):
+                numerator += row[i]
+
+            denominator += row[i]
+
+    return numerator / denominator
 
 # Call the confusionMatrix function and print the confusion matrix as well as the accuracy of the model.
 #The final outputs that we need for this portion of the lab are conf and acc. Copy conf and acc in a .txt file.
@@ -105,7 +160,7 @@ def confusionMatrix(prediction, groundTruth):
 
 groundTruth = pd.Series(groundTruth)
 prediction = pd.Series(predictions)
-conf = #define
-acc = #define
-print 'Accuracy = '+str(acc)+'%'
+conf = confusionMatrix(prediction, groundTruth) #define
+acc = accuracy(conf) #define
+print 'Accuracy = '+str(acc*100)+'%'
 
