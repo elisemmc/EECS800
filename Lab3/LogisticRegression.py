5534b696603ddbbd4bf4f54a49df3b6ef62da8e3
Elise <crimsoncrescentmoon@gmail.com>
2017-03-02 21:33:54 -0600

    working?

--- Lab3/LogisticRegression.py
+++ Lab3/LogisticRegression.py
@@ -19,15 +19,17 @@ for i in X:
     std = X[i].std(axis=0)
     X[i] = ( X[i] - mean )/std
 X.insert(0, "Design", 1.0)
-Xcopy = X.copy()
 Y = df.iloc[:,-1]  
-groundTruth = Y #df['Y']
-#print groundTruth
+groundTruth = Y.copy() #df['Y']
+print groundTruth
 
 #Normalize only if you are using gradient descent. Apply standard deviation for normalization.
-X = np.matrix(X.values, dtype=np.float64)  
-Y = np.matrix(Y.values, dtype=np.float64)
-beta = np.matrix(np.zeros((1,X.shape[1])), dtype=np.float64)
+X = np.matrix(X.values)  
+Y = np.matrix(Y.values)
+beta = np.matrix(np.zeros((1,X.shape[1])))
+
+print X.shape
+print Y.shape
 
 # Define a sigmoid function and return the value that has been calculated for z
 def sigmoid(z):
@@ -38,44 +40,40 @@ def sigmoid(z):
 
     return sig
 
+def hypothesis(beta, X):
+    temp = np.dot(beta, X.transpose())
+    return sigmoid(temp)
 
 # Define the cost function for Logistic Regression. Remember to calculate the sigmoid values as well.
 def costFunction(beta, X, Y):
     '''
     This function returns the value computed from the cost function.
     '''
-    costSum = 0
-
     m = len(X)
-    #print np.sum( np.dot(X[0,:], beta.T) ) 
+
+    costSum = 0
     for i in range(m):
         hypothesis = 1 / ( 1 + np.exp(- np.sum( np.dot(X[i,:], beta.T) )) )
         costSum += Y[:,i] * np.log( hypothesis ) + ( 1-Y[:,i] ) * ( np.log( 1 - hypothesis ) )
-    cost = - costSum/m
+    cost = - costSum
     
     return cost
+
+print "beta"
+print beta
+print costFunction(beta, X, Y)
 
 # # Now , only define the gradient function that we can use in the SciPy's optimize module to find the optimal betas. 
 def gradient(beta, X, Y):
     '''
     This function returns the gradient calucated.
     '''
-    beta = np.matrix(beta)
-    X = np.matrix(X)
-    Y = np.matrix(Y)
-
-    m = len(X)
-
-    parameters = len(X.T)
-    grad = np.zeros(parameters)
-
-    loss = sigmoid( X * beta.T ) - Y
-
-    for j in range(parameters):
-        gradient =  np.multiply(loss, X[:,j])
-        grad[j] = np.sum(gradient)/m
+    grad = np.dot(hypothesis(beta,X)-Y, X)
 
     return grad
+
+print "grad"
+print gradient(beta, X, Y)
 
 # Define a gradient function that takes in beta, X and Y as parameters and returns the best betas and cost. 
 def gradientDescent(X, Y, beta, alpha, iters):
@@ -83,12 +81,15 @@ def gradientDescent(X, Y, beta, alpha, iters):
     Compute the gradient descent function.
     Return the newly computed beta and the cost array.
     '''
-    cost = []
+
+    cost = np.zeros(iters)
 
     for i in range(iters):
-        grad = gradient(beta, X, Y)
-        beta = beta - alpha * grad.T
-        cost.append(costFunction(beta, X, Y))
+        beta = beta - alpha * gradient(beta,X,Y)
+        cost[i] = costFunction(beta, X, Y)
+
+    # print "beta"
+    # print beta
 
     return beta, cost
 
@@ -97,15 +98,14 @@ def gradientDescent(X, Y, beta, alpha, iters):
 alpha = 0.01 #define
 iters = 1000 #define
 result = gradientDescent(X, Y, beta, alpha, iters)
+print "beta"
 print result[0]
-
 
 # Optimize the parameters given functions to compute the cost and the gradients. We can use SciPy's optimization to do the same thing.
 # Define a variable result and complete the functions by adding the right parameters.
 
 #the optimised betas are stored in the first index of the result variable
 result = opt.fmin_tnc(func = costFunction, x0 = beta, fprime = gradient, args=(X,Y))
-print result
 
 
 # Define a predict function that returns 1 if the probablity of the result from the sigmoid function is greater than 0.5, using the best betas and 0 otherwise.
@@ -114,22 +114,30 @@ def predict(beta, X):
     '''
     This function returns a list of predictions calculated from the sigmoid using the best beta.
     '''
-    print beta.T
-    hypothesis = sigmoid(X * beta.T)
-    #print hypothesis
-    prediction = ( 1 if ( x > 0.5 ) else 0  for x in hypothesis )
+    print "predict beta"
+    print beta
+    h = np.array(hypothesis(beta,X))
+    print "h"
+    print h
+    m = len(X)
+    print "m"
+    print m
+    prediction = np.zeros(m)
+
+    for i in range(m):
+        prediction[i] = ( 1 if (h[0,i] > 0.5) else 0 )
+
+    print prediction
+    # print beta.T
+    # hypothesis = sigmoid(X * beta.T)
+    # #print hypothesis
+    # prediction = ( 1 if ( x > 0.5 ) else 0  for x in hypothesis )
 
     return prediction#define
 
 
 # Store the prediction in a list after calling the predict function with best betas and X.
 bestBeta = np.matrix(result[0])
-print "best"
-print bestBeta  
-predictions = predict(bestBeta, X)
-print predictions
-predictionDf = pd.DataFrame()
-predictionDf['predictions'] = predictions
 
 # Calculate the accuracy of your model. The function should take the prediction and groundTruth as inputs and return the 
 # confusion matrix. The confusion matrix is of 'dataframe' type.
@@ -160,15 +168,25 @@ def accuracy(confusionMatrix):
 #Please write a SHORT report and explain these results. Include the explanations for both logistic and linear regression
 #in the same PDF file. 
 
-print ""
-print ""
-print groundTruth.shape
-print predictionDf.shape
+print "best"
+print bestBeta
+predictions = predict(bestBeta, X)
+print "predictions"
+print predictions
+print "groundTruth"
+print np.array(groundTruth)
+# predictionDf = pd.DataFrame()
+# predictionDf['predictions'] = predictions
 
-groundTruth = pd.Series(groundTruth)
-prediction = pd.Series(predictionDf)
+print ""
+print "Y"
+print np.array(Y)[0]
+print 
 
-conf = confusionMatrix(prediction, groundTruth) #define
+# groundTruth = pd.Series(groundTruth)
+# prediction = pd.Series(predictionDf)
+
+conf = confusionMatrix(predictions, np.array(Y)[0]) #define
 print conf
 acc = accuracy(conf) #define
 print 'Accuracy = '+str(acc*100)+'%'
