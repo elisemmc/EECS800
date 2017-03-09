914f3e7824c52933f6582dba5dce8106838d41b9
Elise <crimsoncrescentmoon@gmail.com>
2017-03-09 17:45:40 -0600

    more tests

--- Kaggle1/sci_code.py
+++ Kaggle1/sci_code.py
@@ -4,8 +4,11 @@ import matplotlib.pyplot as plt
 #import statsmodels.api as sm
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_predict
-from sklearn.linear_model import (LinearRegression, Ridge, RidgeCV, MultiTaskLassoCV, TheilSenRegressor, RANSACRegressor, HuberRegressor)
+from sklearn.linear_model import (LinearRegression, Ridge, RidgeCV, 
+    MultiTaskLassoCV, SGDClassifier, SGDRegressor, TheilSenRegressor, 
+    RANSACRegressor, HuberRegressor)
 from sklearn.kernel_ridge import KernelRidge
+from sklearn.metrics import mean_squared_error
 
 def MSE(X, Y):
     '''
@@ -28,7 +31,7 @@ def genCSV4(name, index, latitude, longitude):
     columns = {'lat', 'long'}
     df = pd.DataFrame(result, columns=columns, index=index)
     df.index.name = 'index'
-    name = 'outputs/' + name
+    name = 'outputs/' + name + '.csv'
     df.to_csv(name)
 
 def genCSV(name, index, prediction):
@@ -40,15 +43,133 @@ def genCSV(name, index, prediction):
     columns = {'lat', 'long'}
     df = pd.DataFrame(prediction, columns=columns, index=index)
     df.index.name = 'index'
-    name = 'outputs/' + name
+    name = 'outputs/' + name + '.csv'
     df.to_csv(name)
 
 def genCSV_predtest(name, prediction, truth):
     predcopy = prediction.copy()
     truthcopy = truth.copy()
     content = np.hstack((prediction, truth))
-    name = 'testOutputs/' + name
+    name = 'testOutputs/' + name + '.csv'
     np.savetxt(name, content, delimiter=",")
+
+class Models:
+
+    def __init__(self, X_train, X_test, Y_train, Y_test, X_final, index_final):
+        self.X_train = X_train
+        self.X_test = X_test
+        self.Y_train = Y_train
+        self.Y_test = Y_test
+        self.X_final = X_final
+        self.index_final = index_final
+
+    def ridge(self, name):
+        '''
+        Ridge
+        '''
+        sciRidge = Ridge(
+            alpha= 300, #tested alpha values, alpha=302 has lowest score
+            fit_intercept=True, 
+            normalize=False, 
+            max_iter=None )
+        sciRidge.fit(self.X_train, self.Y_train)
+        predict_test = sciRidge.predict(self.X_test)
+        MSE = mean_squared_error(predict_test, self.Y_test)
+        s = "Sci Ridge              (MSE: %f)" % (MSE)
+        print s    
+        predict_final = sciRidge.predict(self.X_final)
+        genCSV( (name + '_MSE' + str(MSE)), self.index_final, predict_final )
+
+    def ridgeCV(self, name):
+        '''
+        RidgeCV
+        '''
+        sciRidgeCV = RidgeCV(
+            alphas=(0.001, 0.01, 0.1, 1, 2, 5, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320, 340), #tested alpha values, 321 works best
+            fit_intercept=True,
+            cv = 11,
+            normalize=False )
+        sciRidgeCV.fit(self.X_train, self.Y_train)
+        predict_test = sciRidgeCV.predict(self.X_test)
+        MSE = mean_squared_error(predict_test,self.Y_test)
+        s = "Sci RidgeCV            (MSE: %f)" % (MSE)
+        print s
+        predict_final = sciRidgeCV.predict(self.X_final)
+        genCSV( name + '_MSE' + str(MSE), self.index_final, predict_final )
+
+    def lassoCV(self, name):    
+        '''
+        Lasso Regression
+        '''
+        sciLasso = MultiTaskLassoCV( 
+            fit_intercept=True,
+            normalize=False,
+            cv=12,
+            tol = 0.001 )
+        sciLasso.fit(self.X_train, self.Y_train)
+        predict_test = sciLasso.predict(self.X_test)
+        MSE = mean_squared_error(self.X_test,self.Y_test)
+        s = "Sci LassoCV            (MSE: %f)" % (MSE)
+        print s
+        predict_final = sciLasso.predict(self.X_final)
+        genCSV( name + '_MSE' + str(MSE), self.index_final, predict_final )
+
+    def SGDClassifier(self):
+        '''
+        SGD Classifier
+        '''
+        sciSGDLat = SGDClassifier()
+        sciSGDLon = SGDClassifier()
+        Y_trainLat = self.Y_train[:,0].A1
+        Y_trainLon = self.Y_train[:,1].A1
+        Y_trainLat = np.array(["%.2f" % w for w in Y_trainLat])
+        Y_trainLon = np.array(["%.2f" % w for w in Y_trainLon])
+        
+        sciSGDLat.fit(self.X_train, Y_trainLat)
+        sciSGDLon.fit(self.X_train, Y_trainLon)
+
+        predict_testLat = sciSGDLat.predict(self.X_test)#.astype(np.float)
+        predict_testLon = sciSGDLat.predict(self.X_test)#.astype(np.float)
+
+        predict_test = np.hstack((np.matrix(predict_testLat).transpose(), np.matrix(predict_testLon).transpose()))
+        print r2_score(predict_test, self.Y_test)
+
+        # scoreLat = sciSGDLat.score(self.X_test, Y_testLat)
+        # scoreLon = sciSGDLon.score(self.X_test, Y_testLon)
+
+        # s = "Sci SGD              (ScoreLat: %f) (ScoreLat: %f)" % (scoreLat, scoreLon)
+        # print s    
+        #predict_final = sciSGD.predict(self.X_final)
+        #genCSV( 'SciSGD.csv', self.index_final, predict_final )
+
+    def SGDRegressor(self):
+        '''
+        SGD Classifier
+        '''
+        sciSGDLat = SGDRegressor()
+        sciSGDLon = SGDRegressor()
+        Y_trainLat = self.Y_train[:,0].A1
+        Y_trainLon = self.Y_train[:,1].A1
+        Y_testLat = self.Y_test[:,0].A1
+        Y_testLon = self.Y_test[:,1].A1
+        
+        sciSGDLat.fit(self.X_train, Y_trainLat)
+        sciSGDLon.fit(self.X_train, Y_trainLon)
+
+        predict_testLat = sciSGDLat.predict(self.X_test)
+        predict_testLon = sciSGDLat.predict(self.X_test)
+
+        scoreLat = sciSGDLat.score(self.X_test, Y_testLat)
+        scoreLon = sciSGDLat.score(self.X_test, Y_testLat)
+
+
+        predict_test = np.hstack((np.matrix(predict_testLat).transpose(), np.matrix(predict_testLon).transpose()))
+
+
+        s = "Sci SGDRegressor     (ScoreLat: %f) (ScoreLon: %f)" % (scoreLat, scoreLon)
+        print s
+
+
 
 def main():
     trainPredictors = pd.read_csv('InputData/trainPredictors.csv')
@@ -58,6 +179,8 @@ def main():
     pdIndex = trainPredictors.iloc[:,0]
     pdX = trainPredictors.iloc[:,1:]
     pdY = trainTargets.iloc[:,1:]
+    mean = pdY.mean(axis=0)
+    std = pdY.std(axis=0)
     pdX.insert(0, "Design", 1.0)
 
     pdTestIndex = testPredictors.iloc[:,0]
@@ -67,6 +190,8 @@ def main():
     X_orig = np.matrix(pdX.values)
     Y = np.matrix(pdY.values)
     index = np.matrix(pdIndex.values).transpose()
+    #print np.unique(np.array(Y[:,0]))
+    #print np.unique(np.array(Y[:,1]))
 
     X_final_orig = np.matrix(pdTestX.values)
     index_final = np.matrix(pdTestIndex.values).transpose()
@@ -74,79 +199,35 @@ def main():
     '''
     Checking for bad parameters
     '''
-    F=open('blah.txt', 'w')#('parameterCheck(rm5rm7rm30)(0.4).txt', 'a')#
-    #for i in range(X_orig.shape[1]):
-        # print i
-        # if i==(5 or 7):
-        #     i=i+1
-    X = X_orig#np.delete(X_orig, [5,7], axis=1)
-    X_final = X_final_orig#np.delete(X_final_orig, [5,7], axis=1)
+    # F=open('blah.txt', 'w')#('parameterCheck(rm5rm7rm30)(0.4).txt', 'a')#
 
-    #F.write(str(i)+'\n')
+    X = X_orig #np.delete(X_orig, [5,7], axis=1)
+    X_final = X_final_orig #np.delete(X_final_orig, [5,7], axis=1)
 
-    s_Ridge = 0#.204114
-    s_RidgeCV = 0#.203701
-    s_LassoCV = 0#.199943
+    for i in range(100):
+        print i
+        '''
+        Training and Testing Data
+        '''
+        X_train, X_test, Y_train, Y_test = train_test_split( X, Y, test_size=0.5, random_state=i)
 
-    thresh = 0.0001
-    '''
-    Training and Testing Data
-    '''
-    X_train, X_test, Y_train, Y_test = train_test_split( X, Y, test_size=0.1, random_state=0)
-
-    '''
-    Ridge
-    '''
-    sciRidge = Ridge(
-        alpha= 300, #tested alpha values, alpha=302 has lowest score
-        fit_intercept=True, 
-        normalize=False, 
-        max_iter=None )
-    sciRidge.fit(X_train, Y_train)
-    predict_test = sciRidge.predict(X_test)
-    score = sciRidge.score(X_test, Y_test)
-    if score >= s_Ridge + thresh:
-        s = "Sci Ridge              (Score: %f)" % (score)
-        print s
-        F.write(s + '\n')
-    # predict_final = sciRidge.predict(X_final)
-    # genCSV( 'SciRidge.csv', index_final, predict_final )
+        '''
+        Testing Models
+        '''
+        models = Models(X_train, X_test, Y_train, Y_test, X_final, index_final)
+        filename = 'Ridge_test0.5_rand' + str(i)  
+        models.ridge(filename)
+        filename = 'RidgeCV_test0.5_rand' + str(i)
+        models.ridgeCV(filename)
+        #models.lassoCV()
+        #models.SGD()
 
     '''
-    RidgeCV
+    Plotting
     '''
-    sciRidgeCV = RidgeCV(
-        alphas=(0.001, 0.01, 0.1, 1, 2, 5, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320, 340), #tested alpha values, 321 works best
-        fit_intercept=True,
-        cv = 11,
-        normalize=False )
-    sciRidgeCV.fit(X_test, Y_test)
-    predict_test = sciRidgeCV.predict(X_test)
-    score = sciRidgeCV.score(X_test,Y_test)
-    if score >= s_RidgeCV + thresh:
-        s = "Sci RidgeCV            (Score: %f)" % (score)
-        print s
-        F.write(s + '\n')
-    predict_final = sciRidgeCV.predict(X_final)
-    genCSV( 'SciRidgeCV.csv', index_final, predict_final )
-
-    '''
-    Lasso Regression
-    '''
-    sciLasso = MultiTaskLassoCV( 
-        fit_intercept=True,
-        normalize=False,
-        cv=12,
-        tol = 0.0001 )
-    sciLasso.fit(X, Y)
-    predict_test = sciLasso.predict(X_test)
-    score = sciLasso.score(X_test,Y_test)
-    if score >= s_LassoCV + thresh:
-        s = "Sci LassoCV            (Score: %f)" % (score)
-        print s
-        F.write(s + '\n')
-    predict_final = sciLasso.predict(X_final)
-    genCSV( 'sciLasso.csv', index_final, predict_final )
+    #for i in range(X.shape[1]):
+    #    plt.scatter(np.array(Y[:,0]),np.array(X[:,i]), alpha=0.1)
+    #plt.show()
 
 
 
