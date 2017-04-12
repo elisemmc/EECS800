dfd9c92acbffaa2d62e8683aaa0302be3e8db17b
Elise <crimsoncrescentmoon@gmail.com>
2017-04-11 22:40:01 -0500

    Lab 5 expectation and posterior

--- lab5/EM.py
+++ lab5/EM.py
@@ -7,6 +7,7 @@ import pandas as pd
 import random as rand
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+from scipy.stats import multivariate_normal
 from sys import maxint
 
 #Please complete the following Expectation Maximization code, implementing a batch EM is sufficient for this lab
@@ -21,11 +22,9 @@ sig = [ [3, 0], [0, 4] ]
 muPrime = [6, 0]
 sigPrime = [ [5, 0], [0, 2] ]
 
-
 #Generate samples of type MVN and size 100 using mu/sigma and muPrime/sigmaPrime. 
-x1, y1 = #MVN sample of size 100 using mu and sigma
-x2, y2 = #MVN sample of size 100 using muPrime and sigmaPrime
-
+x1, y1 = np.random.multivariate_normal(mu, sig, 100).T #MVN sample of size 100 using mu and sigma
+x2, y2 = np.random.multivariate_normal(muPrime, sigPrime, 100).T #MVN sample of size 100 using muPrime and sigmaPrime
 
 x = np.concatenate((x1, x2))
 y = np.concatenate((y1, y2))
@@ -45,13 +44,47 @@ initialGuess = { 'mu': [2,2], 'sig': [ [1, 0], [0, 1] ], 'muPrime': [5,5], 'sigP
 
 # Compute the posterior with the help of computing the pdf, e.g. using norm.pdf
 def posterior(val, mu, sig, alpha):
- '''posteriors'''
-  return prob
+  '''posteriors'''
+  probs = np.zeros( ( val.shape[0], len(alpha) ) )
+  num_classes = len(alpha)
+  sums = 0
 
+  for c in range(num_classes):
+  	a = alpha[c]
+   	m = mu[c]
+   	s = sig[c]
+	pdf =  multivariate_normal.pdf(x=val, mean=m, cov=s)
+	sums += a * pdf
+	probs[:,c] = a*pdf
+
+  for c in range(num_classes):
+  	probs[:,c] = np.divide( probs[:,c], sums )
+  
+  print probs
+  #print np.sum(probs, axis=1)
+  return probs 
 
 # The E-step, estimate w, this w is the "soft guess" step for the class labels. You have to use the already defined posteriors in this step.
 def expectation(dataFrame, parameters):
-  '''This function uses the posterios to estimate w.'''
+  '''This function uses the posteriors to estimate w.'''
+
+  data = dataFrame.as_matrix()
+  alpha = parameters['alpha'].tolist()
+  
+  mu_1 = parameters['mu'].tolist()
+  sig_1 = parameters['sig'].tolist()
+
+  mu_2 = parameters['muPrime'].tolist()
+  sig_2 = parameters['sig'].tolist()
+
+  mu = (mu_1, mu_2)
+  sig = (sig_1, sig_2)
+
+  posterior(data, mu, sig, alpha)
+  
+  # print multivariate_normal.pdf(x=data[0], mean=mu_1, cov=sig_1)
+  
+
   return #dataframe with the soft guess for the labels
 
 
@@ -72,8 +105,10 @@ def maximization(dataFrame, parameters):
 iters = 0
 params = pd.DataFrame(initialGuess)
 
-while ###:
+while iters < 1:
   iters += 1
+
+  expectation(df, params)
   # E-step
  
   # M-step
